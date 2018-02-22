6099c52fa547970bc78fbd29d99b9adaa10b7c20
=vmarcosp <=contato@vmarcosp.com.br>
2018-02-22 16:22:45 -0300

    Added new files

---
+++ bs-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  port: 3005,
+  server: {
+    baseDir: './demo'
+  }
+};
---
+++ src/main.js
@@ -0,0 +1,94 @@
+import { stringify as querify, parse } from 'qs'
+
+const URL_AUTHORIZE = 'https://github.com/login/oauth/authorize'
+
+class GithubOAuthWrapper {
+  constructor (clientId, redirectUri, urlApiAuthentication) {
+    this._clientId = clientId
+    this._redirectUri = redirectUri
+    this._urlApiAuthentication = urlApiAuthentication
+
+    this._popup = null
+  }
+
+  execute () {
+    return this
+      ._openPopup()
+      ._setFocusPopup()
+      ._validateOAuthCallback()
+      .then(response => {
+        if (this._urlApiAuthentication) {
+          return 'token....'
+        }
+
+        return response
+      })
+  }
+
+  _openPopup () {
+    const url = this._createUrl()
+    const popupProperties = this._getPopupProperties()
+
+    this._popup = window.open(url, 'Github Authentication', querify(popupProperties, {
+      delimiter: ', '
+    }))
+
+    return this
+  }
+
+  _getPopupProperties () {
+    const screen = window.screen
+    const width = 500
+    const height = 800
+    const popupProperties = {
+      width,
+      height,
+      top: (screen.width / 2) - (width / 2),
+      left: (screen.height / 2) - (height / 2)
+    }
+
+    return popupProperties
+  }
+
+  _setFocusPopup () {
+    if (this._popup.focus) {
+      this._popup.focus()
+    }
+
+    return this
+  }
+
+  _validateOAuthCallback () {
+    return new Promise((resolve, reject) => {
+      const interval = setInterval(() => {
+        // TODO Tratar fechamento do popup
+        try {
+          const splittedUrl = this._popup.location.href.split('?')
+          const currentHost = splittedUrl[0]
+          const params = splittedUrl[1]
+
+          if (currentHost === this._redirectUri) {
+            this._popup.close()
+            clearInterval(interval)
+            resolve(parse(params))
+          }
+        } catch (error) {
+          // Log...
+        }
+      }, 250)
+    })
+  }
+
+  _createUrl () {
+    const params = {
+      'response_type': 'code',
+      'client_id': this._clientId,
+      'redirect_uri': this._redirectUri,
+      'scope': 'user:email'
+    }
+
+    return `${URL_AUTHORIZE}?${querify(params)}`
+  }
+}
+
+module.exports = GithubOAuthWrapper
